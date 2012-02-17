083ac8f05a0f5166f4c6b8d793d8a5987ae0152a
Roman Birg <romanbirg@gmail.com>
2012-02-17 00:50:45 -0600

    move over from vendor

--- full_toro.mk
+++ full_toro.mk
@@ -34,17 +34,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from toro device
 $(call inherit-product, device/samsung/toro/device.mk)
 
-# Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_toro
+# Inherit common product files.
+$(call inherit-product, vendor/aokp/products/common_phone.mk)
+
+# Tuna overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/tuna
+PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/toro
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_toro
+PRODUCT_BRAND := google
 PRODUCT_DEVICE := toro
-PRODUCT_BRAND := Google
 PRODUCT_MODEL := Galaxy Nexus
-PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRODUCT_NAME=mysid \
-	BUILD_ID=IML74K \
-	BUILD_DISPLAY_ID=IML74K \
-	BUILD_FINGERPRINT="google/mysid/toro:4.0.2/ICL53F/235179:user/release-keys" \
-	PRIVATE_BUILD_DESC="mysid-user 4.0.2 ICL53F 235179 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mysid BUILD_ID=ICL53F BUILD_FINGERPRINT=google/mysid/toro:4.0.2/ICL53F/235179:user/release-keys PRIVATE_BUILD_DESC="mysid-user 4.0.2 ICL53F 235179 release-keys" BUILD_NUMBER=235179
 
+# Inherit common build.prop overrides
+-include vendor/aokp/products/common_versions.mk
+
+# Copy toro specific prebuilt files
+PRODUCT_COPY_FILES +=  \
+    vendor/aokp/prebuilt/tuna/Thinkfree.apk:system/app/Thinkfree.apk \
+    vendor/aokp/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip
